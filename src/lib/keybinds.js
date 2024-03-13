c13f99861ef1ea71398b8da08af21325d9990cd7
fiecueal <fiecueal@proton.me>
2024-03-12 17:13:41 -0700

    reverse keys and vals

--- src/lib/keybinds.js
+++ src/lib/keybinds.js
@@ -3,16 +3,16 @@
 // (that can be done with one side of the
 // keyboard instead of all over the place)
 export let keybinds = {
-  q: "linecap",
-  w: "linejoin",
-  e: "mirror",
-  r: "fill",
-  a: "line",
-  s: "arc",
-  d: "arc rev",
-  f: "bezier",
-  z: "close",
-  x: "remove point",
-  c: "", // not assigned in dotgrid
-  v: "", // not assigned in dotgrid
+  linecap: "q",
+  linejoin: "w",
+  mirror: "e",
+  fill: "r",
+  line: "a",
+  arc: "s",
+  arc_rev: "d",
+  bezier: "f",
+  close: "z",
+  remove_point: "x",
+  // c: "", // not assigned in dotgrid
+  // v: "", // not assigned in dotgrid
 }
